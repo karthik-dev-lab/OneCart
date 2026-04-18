c493d9abbfa943534ef81fed06a17d05848f618e
Kokkula Karthik <karthiikk8348@gmail.com>
2026-04-18 18:30:44 +0530

    Fix CORS origin format in index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ["https://onecart-1-37eg.onrender.com/"],
+    origin: "https://onecart-1-37eg.onrender.com",
     credentials: true
 }))
 
